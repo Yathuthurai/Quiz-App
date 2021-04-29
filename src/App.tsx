711cebbc159b270142741ee6d356132c810fae56
Yathurshan <tyathurshan@gmail.com>
2021-04-29 15:14:47 +0530

    Implemented next question function

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,14 @@ const App = () => {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className="app">
